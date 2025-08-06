9230a432a3c1b559161236d8c0016c4c66b74191
Lkshjnj <lakshyajuneja22@gmail.com>
2025-08-06 18:56:19 +0000

    First LMS commit

---
+++ backend/users/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class Role(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class CustomUser(AbstractUser):
+    role = models.ForeignKey(Role, on_delete=models.SET_NULL, null=False, blank=False)
+
+    def __str(self):
+        return self.username
