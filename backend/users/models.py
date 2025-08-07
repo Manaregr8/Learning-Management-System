ba8cb3f01190457a267428d0867ac890207bf52e
Lkshjnj <lakshyajuneja22@gmail.com>
2025-08-07 10:17:02 +0000

    second_commit

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager 
 
 class Role(models.Model):
     name = models.CharField(max_length=50, unique=True)
@@ -7,8 +7,43 @@ class Role(models.Model):
     def __str__(self):
         return self.name
 
-class CustomUser(AbstractUser):
-    role = models.ForeignKey(Role, on_delete=models.SET_NULL, null=False, blank=False)
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, role=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, role=role)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-    def __str(self):
-        return self.username
+    def create_superuser(self, username, email, password):
+        user = self.create_user(username, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+    
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True)
+    password_hash = models.TextField()
+    role = models.ForeignKey(Role, on_delete=models.CASCADE, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+
+    def save(self, *args, **kwargs):
+        if not self.password_hash:
+            self.set_password(self.password)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.email
