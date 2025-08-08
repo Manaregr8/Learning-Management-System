9460ca17d6d357ec76cebfe3528c476c5fa05456
Manjeet Singh <99804338+Manaregr8@users.noreply.github.com>
2025-08-08 13:22:29 +0530

    Update urls.py
    added check-role path in url patterns

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,8 +1,8 @@
-# backend/users/urls.py
 from django.urls import path
-from .views import LoginView, RegisterUserView
+from .views import LoginView, RegisterUserView, CheckUserRoleView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('register/', RegisterUserView.as_view(), name='register'),
+    path('check-role/', CheckUserRoleView.as_view(), name='check-role'),
 ]
