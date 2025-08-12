56f16bbc6843ff67bf12510004eaacd1ec298d46
Manaregr8 <manjeetdevelops@gmail.com>
2025-08-12 17:48:54 +0530

    added admin and functionality

---
+++ frontend/src/app/dashboard/trainer/page.js
@@ -0,0 +1,8 @@
+export default function TeacherDashboard() {
+  return (
+    <main>
+      <h1>Teacher Dashboard</h1>
+      <p>Here you can manage your classes and assignments.</p>
+    </main>
+  );
+}
