ac486508a9b05a076ab786501c29046c0251b889
Lkshjnj <lakshyajuneja22@gmail.com>
2025-08-15 09:39:54 +0000

    added views and urls and permission classes

--- backend/courses/views.py
+++ backend/courses/views.py
@@ -1,17 +1,33 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import *
 from .serializers import *
+from .permissions import *
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [isAuthenticatedGetAndAdminPost]
 
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+    permission_classes = [isAuthenticatedGetAndAdminPost]
 
-class ModuleViewSet(viewsets.ModelViewSet):
-    queryset = Modules.objects.all()
+    @action(detail=True, methods=['get'])
+    def modules(self, request, pk=None):
+        course = self.get_object()
+        modules = course.module.all()
+        serializer = ModuleSerializer(modules, many=True)
+        return Response(serializer.data)
     
-# Create your views here.
+    @action(detail=True, methods=['get'])
+    def lessons(self, request, pk=None):
+        course = self.get_object()
+        lessons = Lessons.objects.filter(module__courses=course)
+        serializer = LessonSerializer(lessons, many=True)
+        return Response(serializer.data)
+
+
