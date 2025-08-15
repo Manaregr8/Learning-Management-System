c5b7adc0c485e4282b347b96b1f7d0f4b7ce6939
Lkshjnj <lakshyajuneja22@gmail.com>
2025-08-15 10:19:57 +0000

    added permission for trainers on courses

--- backend/courses/permissions.py
+++ backend/courses/permissions.py
@@ -4,6 +4,6 @@ class isAuthenticatedGetAndAdminPost(BasePermission):
     def has_permission(self, request, view):
         if request.method in SAFE_METHODS:
             return request.user and request.user.is_authenticated
-        return request.user and request.user.is_staff
+        return request.user and (request.user.is_staff or request.user.role.name.lower() == 'trainer')
     
 
